fe2721de3d989721187fbf0d68a1c95464d5e243
Carlos Neto <carlos.neto.dev@gmail.com>
2021-02-04 23:46:29 -0300

    Update app.py

--- query-geojson-mongo-db/app.py
+++ query-geojson-mongo-db/app.py
@@ -8,3 +8,27 @@ mongo = MongoClient(
 )
 db = mongo['test_database']
 db['testcol'].insert_one({'1': '2'})
+
+
+# db.partner.find( 
+# { $and: [ 
+#             {    coverageArea: {
+#                     "$geoIntersects":{
+#                         "$geometry":{
+#                             "type":"Point", "coordinates":[34.5609, 27.2900]
+#                         }
+#                     }
+#                 }
+#             },
+            
+#             {     address: {
+#                     "$near": {
+#                         "$geometry": { 
+#                             "type": "Point", "coordinates": [-46.57421, -21.785741] 
+#                             }
+#                          }
+#                      }
+#             }
+#         ] 
+#     } 
+# )
